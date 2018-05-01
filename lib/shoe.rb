ebdf5b1eb9e3fb8d0de072feb0e4416ddbeb150f
Spencer Goodman <spencerwgoodman@icloud.com>
2018-05-01 01:37:03 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,6 +1,7 @@
 # Make your shoe class here!
 
 class Shoe
+  attr_accessor :
 
   def initialize(brand)
     @brand = brand
@@ -8,30 +9,6 @@ class Shoe
 
   def brand
     @brand
-  end
-
-  def color=(color)
-    @color = color
-  end
-
-  def color
-    @color
-  end
-
-  def material=(material)
-    @material = material
-  end
-
-  def material
-    @material
-  end
-
-  def condition=(condition)
-    @condition = condition
-  end
-
-  def condition
-    @condition
   end
 
   def cobble
